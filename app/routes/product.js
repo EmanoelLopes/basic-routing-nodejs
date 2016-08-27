db3473868c0bd9ca15e38cc41f1085fe0e387a1a
Emanoel <emanoel.lopes.web@gmail.com>
2016-08-27 16:23:02 -0300

    Renamed files

--- app/routes/product.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(app) {
-	app.get('/products', function (req, res) {
-	 res.render(
-			'products/product',
-			{ title: 'Product page', message: 'Thats the product page!'}
-	 	);
-	});
-};
